0d08853750f5cebcecfef9477ea7e0f821f5af12
cmasenv <greendogles@gmail.com>
2020-02-26 22:04:35 +0000

    Initial commit, beginnings of BlockBreaker game. Initial assets moved in.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour {
+
+    [SerializeField] string nextScene;
+
+    public void LoadNextSceneByName() {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void LoadNextSceneByIndex() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
